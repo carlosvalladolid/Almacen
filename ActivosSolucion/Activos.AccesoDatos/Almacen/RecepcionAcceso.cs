6bfbad038182c2c1f5bd8016161c2434c7186b73
Xhulady <xhulady@hotmail.es>
2014-11-13 13:39:09 -0600

    s agregaon los campos en la entidad de recepcion

--- ActivosSolucion/Activos.AccesoDatos/Almacen/RecepcionAcceso.cs
+++ ActivosSolucion/Activos.AccesoDatos/Almacen/RecepcionAcceso.cs
@@ -2,10 +2,56 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+using Activos.Entidad.General;
+using Activos.Entidad.Catalogo;
+using Activos.Entidad.Almacen;
+using Activos.Comun.Constante;
 
 namespace Activos.AccesoDatos.Almacen
 {
-    class RecepcionAcceso
+ public   class RecepcionAcceso:Base
     {
+
+ protected int _ErrorId;
+        protected string _DescripcionError;
+
+        /// <summary>
+        ///     Número de error, en caso de que haya ocurrido uno. Cero por default.
+        /// </summary>
+        public int ErrorId
+        {
+            get { return _ErrorId; }
+        }
+
+        /// <summary>
+        ///     Descripción de error, en caso de que haya ocurrido uno. Empty por default.
+        /// </summary>
+        public string DescripcionError
+        {
+            get { return _DescripcionError; }
+        }
+
+        /// <summary>
+        ///     Constructor de la clase.
+        /// </summary>
+        public RecepcionAcceso()
+        {
+            _ErrorId = 0;
+            _DescripcionError = string.Empty;
+        }
+
+        #region "Métodos"
+
+
+
+
+
+        #endregion
+
+
+
     }
 }
--- ActivosSolucion/Activos.Entidad/Almacen/RecepcionEntidad.cs
+++ ActivosSolucion/Activos.Entidad/Almacen/RecepcionEntidad.cs
@@ -5,7 +5,125 @@ using System.Text;
 
 namespace Activos.Entidad.Almacen
 {
-    class RecepcionEntidad
+  public  class RecepcionEntidad:Base
     {
+
+        //campos de RecepcionEncabezado
+    public string  _RecepcionId;
+	public string _OrdenId;
+	public Int32 _EmpleadoId;
+	public Int32 _JefeId;
+	public Int16 _ProveedorId;
+	public Int16 _TipoDocumentoId ;
+	public Int16 _EstatusId;
+	public string _Clave;
+	public string _FechaDocumento;
+	public string _Monto;
+    //campos de RecepcionDetalle
+	public string _ProductoId;
+	public string _Precio;
+    public string _Cantidad;
+
+    public RecepcionEntidad()
+        {
+            _RecepcionId = string.Empty;
+            _OrdenId = string.Empty;
+            _EmpleadoId = 0;
+            _JefeId = 0;
+            _ProveedorId = 0;
+            _TipoDocumentoId = 0;
+            _EstatusId = 0;
+            _Clave = string.Empty;
+            _FechaDocumento = string.Empty;
+            _Monto = string.Empty;
+            _ProductoId = string.Empty;
+            _Precio = string.Empty;
+            _Cantidad = string.Empty;
+
+        }
+
+    public string RecepcionId
+    {
+        get { return _RecepcionId; }
+        set { _RecepcionId = value; }
+    }
+
+    public string OrdenId 
+    {
+        get { return _OrdenId; }
+        set { _OrdenId = value; }
+    }
+      
+    public Int32 EmpleadoId
+    {
+        get { return _EmpleadoId; }
+        set { _EmpleadoId = value; }
+    }
+
+    public Int32 JefeId
+    {
+        get { return _JefeId; }
+        set { _JefeId = value; }
+    }
+
+    public Int16 ProveedorId
+    {
+        get { return _ProveedorId; }
+        set { _ProveedorId = value; }
+    }
+
+    public Int16 TipoDocumentoId
+    {
+        get { return _TipoDocumentoId; }
+        set { _TipoDocumentoId = value; }
+    }
+
+
+    public Int16 EstatusId
+    {
+        get { return _EstatusId; }
+        set { _EstatusId = value; }
+    }
+
+
+    public string Clave
+    {
+        get { return _Clave; }
+        set { _Clave = value; }
+    }
+
+
+    public string FechaDocumento
+    {
+        get { return _FechaDocumento; }
+        set { _FechaDocumento = value; }
+    }
+
+    public string Monto
+    {
+        get { return _Monto; }
+        set { _Monto = value; }
+    }
+
+    public string ProductoId
+    {
+        get { return _ProductoId; }
+        set { _ProductoId = value; }
+    }
+
+
+    public string Precio
+    {
+        get { return _Precio; }
+        set { _Precio = value; }
+    }
+
+    public string Cantidad
+    {
+        get { return _Cantidad; }
+        set { _Cantidad = value; }
+    }
+
+
     }
 }
